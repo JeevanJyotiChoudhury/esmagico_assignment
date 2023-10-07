6d7617e63a78758bdb1e6f8466bf6981647f02d5
Jeevan Jyoti Choudhury <jeevanjyoti549@gmail.com>
2023-10-07 15:57:32 +0530

    banner

--- src/components/AboutSpeakin.jsx
+++ src/components/AboutSpeakin.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import React from "react";
+import watermarkbg from "../assets/Rectangle 196wm5.png";
+import watermarkbg2 from "../assets/open-mic 1.png";
+import micbg from "../assets/Screenshot 2020-11-16 at 1.08bg.png";
+import mic from "../assets/unnamed 1.png";
 
 const AboutSpeakin = () => {
   return (
-    <div>AboutSpeakin</div>
-  )
-}
+    <Box
+      backgroundImage={watermarkbg}
+      backgroundRepeat="no-repeat"
+      backgroundSize="cover"
+      backgroundPosition="center"
+      position="relative"
+    >
+      <Image src={watermarkbg2} boxSize="80vh" objectFit="contain" w="100%" />
+      <Box position="absolute" top="24" w="100%">
+        <Flex>
+          <Box
+            w="40%"
+            backgroundImage={micbg}
+            backgroundRepeat="no-repeat"
+            backgroundSize="cover"
+            backgroundPosition="center"
+            h="60vh"
+          >
+            <Image src={mic} objectFit="cover" />
+            {/* <Image src={mic} /> */}
+          </Box>
+          <Box w="60%">
+            <Text>About SpeakIn</Text>
+            <Text>
+              SpeakIn is Asia's largest on-demand network providing access to
+              top regional speakers, thought leaders and their content. Enabling
+              organizations to build performance agility and win over
+              disruption, at SpeakIn only the best come together to inform,
+              inspire and motivate.
+            </Text>
+          </Box>
+        </Flex>
+      </Box>
+    </Box>
+  );
+};
 
-export default AboutSpeakin
+export default AboutSpeakin;
